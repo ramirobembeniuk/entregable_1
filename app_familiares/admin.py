05a9b514bd587f897e0cfaebbacd3a355f585e34
ramiro bembeniuk <ramirobembeniuk@gmail.com>
2022-05-31 17:35:02 -0300

    models

--- app_familiares/admin.py
+++ app_familiares/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from app_familiares.models import App_Familiares
+from app_familiares.models import App_Familiares, Categoria
 
 # Register your models here.
 admin.site.register(App_Familiares)
+admin.site.register(Categoria)
--- app_familiares/models.py
+++ app_familiares/models.py
@@ -5,3 +5,11 @@ class App_Familiares(models.Model):
     fecha_nacimiento = models.CharField(max_length=100)
     observaciones = models.CharField(max_length=200, blank=True, null=True)
     active = models.BooleanField(default=True)
+
+class Meta:
+    verbose_name = 'App_Familiares'
+    verbose_name_plural = 'App_Familiares'
+
+class Categoria(models.Model):
+    name = models.CharField(max_length=50)
+    description = models.CharField(max_length=100)
